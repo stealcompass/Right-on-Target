58fbe7b56615bc95acbc7a7a9034cefece8bccf0
Veniamin Ermolaev <venya.e@gmail.com>
2023-01-28 15:27:29 +0300

    Initial Commit

---
+++ Right on Target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on Target
+//
+//  Created by Veniamin on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
